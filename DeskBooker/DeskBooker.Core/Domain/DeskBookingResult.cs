2fb6662051a4d0fc62323b6b5cf3d50db834916d
Jakob Hendrix <jhendrix001@gmail.com>
2021-08-18 18:30:37 -0500

    First refactor step - moved generated code to domain library

--- DeskBooker/DeskBooker.Core/Domain/DeskBookingResult.cs
+++ DeskBooker/DeskBooker.Core/Domain/DeskBookingResult.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace DeskBooker.Core.Processor
+namespace DeskBooker.Core.Domain
 {
-    public class DeskBookingRequest
+    public class DeskBookingResult
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
