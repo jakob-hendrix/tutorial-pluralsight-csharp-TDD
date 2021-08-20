bba78cf03a39783170af56ace91df4ee0ebe13d8
Jakob Hendrix <jhendrix001@gmail.com>
2021-08-20 13:41:57 -0500

    Next green  - Save() with mocking the data access repo with Moq

--- DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker/DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using DeskBooker.Core.DataInterface;
 using DeskBooker.Core.Domain;
 
 namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessor
     {
+        private readonly IDeskBookingRepository _deskBookingRepository;
+
+        public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository)
+        {
+            _deskBookingRepository = deskBookingRepository;
+        }
+
         public DeskBookingResult BookDesk(DeskBookingRequest request)
         {
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            _deskBookingRepository.Save(new DeskBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            });
 
             return new DeskBookingResult
             {
